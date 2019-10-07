c29e620c5dad7bb54e4ea0a36a5563323a0cdd87
League Student <league@iMac-55.attlocal.net>
2019-10-06 17:30:23 -0700

    Day 1

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,10 +1,49 @@
 package arrays;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
-	//1. make a main method
+	static boolean done;
+
+	public static void main(String[] args) {
+		Robot robs[] = new Robot[5];
+		for(int i = 0; i < 5; i++) {
+			robs[i] = new Robot();
+			//robot[i].robot();
+			robs[i].setX((i+1)*150);
+			robs[i].setY(450);
+			System.out.println("Creating Bot # " + i);
+		}
+		while(!done) {
+			for(int i = 0; i < 5; i++) {
+				//robs[i] = new Robot();
+				//robot[i].robot();
+				robs[i].setSpeed(500);
+				robs[i].move(random(50));
+				System.out.println("Creating Bot # " + i);
+				if(robs[i].getY() < 1 && done == false) {
+					done = true;
+					JOptionPane.showMessageDialog(null, "Bot "+(i+1) +" Won");
+				}
+			}	
+		}
+
+	}
+	static int random(int amt) {
+
+		Random rand = new Random();
+		
+		return rand.nextInt(amt);
+		
+		
+	
+	}
 
 	//2. create an array of 5 robots.
-
+	
 	//3. use a for loop to initialize the robots.
 
 		//4. make each robot start at the bottom of the screen, side by side, facing up
